216acdf4d3361f0c9d139ae9401100baaf6f188b
guanzhisong <guanzhisong@gmail.com>
2021-07-01 19:19:38 +0800

    feat:modify index.vue

--- src/views/monitor/perflog/index.vue
+++ src/views/monitor/perflog/index.vue
@@ -81,14 +81,12 @@
     </el-form>
     -->
 
-    <PerfSearch />
+    <PerfSearch ref="searchForm"/>
 
-    <el-form :model="queryParams">
-      <el-form-item>
+    <div>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
         <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
-      </el-form-item>
-    </el-form>
+    </div>
 
     <el-table
       v-loading="loading"
@@ -135,8 +133,8 @@
     <pagination
       v-show="total>0"
       :total="total"
-      :page.sync="queryParams.pageNum"
-      :limit.sync="queryParams.pageSize"
+      :page.sync="pageParams.pageNum"
+      :limit.sync="pageParams.pageSize"
       @pagination="getList"
     />
 
@@ -200,59 +198,34 @@ export default {
         // 服务器IP
         // operatorIps: []
       },
-      // 时间筛选
-      dateRange: [],
-      // 查询参数
-      queryParams: {
-        pageNum: 1,
-        pageSize: 10,
-        id: undefined,
-        product: undefined,
-        group: undefined,
-        app: undefined,
-        clazz: undefined,
-        method: undefined,
-        // operatorIps: undefined,
-        level: undefined
-      },
       // 表单参数
-      form: {},
+      form: {
+
+      },
+      pageParams: {
+        pageNum: 1,
+        pageSize: 10
+      },
       // 明细日志
       detailMsg: '',
       // 表单校验
       rules: {
+
       }
     };
   },
   computed: {
-    timeDefault() {
-      let date = new Date();
-      // 通过时间戳计算
-      let defalutStartTime = date.getTime() - 1 * 24 * 3600 * 1000;// 转化为时间戳
-      let defalutEndTime = date.getTime();
-      let startDateNs = new Date(defalutStartTime);
-      let endDateNs = new Date(defalutEndTime);
-      // 月，日 不够10补0
-      defalutStartTime = startDateNs.getFullYear() + '-' + ((startDateNs.getMonth() + 1) >= 10 ? (startDateNs.getMonth() + 1) : '0' + (startDateNs.getMonth() + 1)) + '-' + (startDateNs.getDate() >= 10 ? startDateNs.getDate() : '0' + startDateNs.getDate()) + " 00:00:00";
-      defalutEndTime = endDateNs.getFullYear() + '-' + ((endDateNs.getMonth() + 1) >= 10 ? (endDateNs.getMonth() + 1) : '0' + (endDateNs.getMonth() + 1)) + '-' + (endDateNs.getDate() >= 10 ? endDateNs.getDate() : '0' + endDateNs.getDate()) + " 00:00:00";
-      return [defalutStartTime, defalutEndTime];
-    }
-  },
-  //页面加载时候，在mounted中进行赋值
-  mounted() {
-    // 初始化查询，默认为前一天
-    this.dateRange = this.timeDefault;
+   
   },
   created() {
-    this.dateRange = this.timeDefault;
-    this.getProducts();
     this.getList();
   },
   methods: {
     /** 查询系统接口日志列表 */
     getList() {
       this.loading = true;
-      listLog(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
+      let mergeParams = Object.assign(this.pageParams,this.$refs.searchForm.queryParams);
+      listLog(this.addDateRange(mergeParams, this.$refs.searchForm.dateRange)).then(response => {
         this.logList = response.rows;
         this.total = response.total;
         this.loading = false;
@@ -268,6 +241,7 @@ export default {
       this.form = {
         id: undefined,
         metaId: undefined,
+        operatorIp:undefined,
         executeTimespan: undefined,
         paramsIn: undefined,
         paramsOut: undefined,
@@ -280,7 +254,7 @@ export default {
     },
     /** 搜索按钮操作 */
     handleQuery() {
-      this.queryParams.pageNum = 1;
+      this.pageParams.pageNum = 1;
       this.getList();
     },
     /** 重置按钮操作 */
@@ -299,76 +273,9 @@ export default {
       // 4  8 9 11列的数据较长,需要弹出对话框显示
       const index = column.index;
       if (index === 4 || index === 8 || index === 9 || index === 11) {
-        // JSON.stringify(cell);
         this.open = true;
         this.detailMsg = cell.innerText;
       }
-    },
-
-    // 获取产品线
-    getProducts() {
-      const query = {
-        level: 1
-      };
-      getMetaLog(query).then(response => {
-        this.option.products = response.data;
-        this.option.products.push('*');
-      });
-    },
-
-    // 产品线
-    handleProductChange() {
-      const query = {
-        level: 2,
-        product: this.queryParams.product
-      };
-      getMetaLog(query).then(response => {
-        this.option.groups = response.data;
-        this.option.groups.push('*');
-      });
-    },
-
-    // 服务组
-    handleGroupChange() {
-      const query = {
-        level: 3,
-        product: this.queryParams.product,
-        group: this.queryParams.group
-      };
-      getMetaLog(query).then(response => {
-        this.option.apps = response.data;
-        this.option.apps.push('*');
-      });
-    },
-
-    // 应用
-    handleAppChange() {
-      const query = {
-        level: 4,
-        product: this.queryParams.product,
-        group: this.queryParams.group,
-        app: this.queryParams.app
-      };
-      getMetaLog(query).then(response => {
-        this.option.clazzs = response.data;
-        this.option.clazzs.push('*');
-      });
-    },
-
-    // 方法
-    handleClazzChange() {
-      this.queryParams.level = 5;
-      const query = {
-        level: 5,
-        product: this.queryParams.product,
-        group: this.queryParams.group,
-        app: this.queryParams.app,
-        clazz: this.queryParams.clazz
-      };
-      getMetaLog(query).then(response => {
-        this.option.methods = response.data;
-        this.option.methods.push('*');
-      });
     },
 
     // ip
